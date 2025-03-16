7b6ba2e77fb8f41b6e56aeca14f25923635e32d8
Brahad <94699627+brahad316@users.noreply.github.com>
2025-03-16 14:19:46 +0530

    Updated input.v

--- input.v
+++ input.v
@@ -1,8 +1,10 @@
-if(x >= 5) 
+if(x < -  5) 
     begin 
-    p <= 73;
+    p <= 73  ;
     end
 else 
     begin
-    p <= 37;
+    p     <= -37;
     end
+
+// Dirty code for testing purposes only, please don't ever write in such a way or you may be subjected to be punished by the programming gods.
