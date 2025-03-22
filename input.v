b19538f6eec372ba37b8e5ab35f546fe6c2401de
Brahad <94699627+brahad316@users.noreply.github.com>
2025-03-22 17:59:46 +0530

    Update input.v

--- input.v
+++ input.v
@@ -1,10 +1,8 @@
-if(x < -5) 
-    begin 
-    p <= 73  ;
+if ((Comp_V <= 5))
+    begin
+      My_assign_Var <= -73;  
     end
 else 
     begin
-    p     <= -37;
+      My_assign_Var <= 37;
     end
-
-// Dirty code for testing purposes only, please don't ever write in such a way or you may be subjected to be punished by the programming gods.
